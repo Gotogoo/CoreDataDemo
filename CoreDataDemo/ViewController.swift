7557a8aeadc5165ccef49bf7bd0a49ed74962fb9
Facheng Liang <fcliang@thoughtworks.com>
2018-01-13 23:24:40 +0800

    Initial Commit

---
+++ CoreDataDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreDataDemo
+//
+//  Created by Facheng Liang  on 13/01/2018.
+//  Copyright © 2018 Facheng Liang . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
